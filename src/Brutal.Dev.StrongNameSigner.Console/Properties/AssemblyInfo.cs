6c43627f3334c5cf64d6f1592b96cfd9c928269b
Werner van Deventer <werner@brutaldev.com>
2017-05-01 10:27:46 +0200

    Revert back to stable Mono.Cecil because the new version (beta5) locks files on disk (issue #35).

--- src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
+++ src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://brutaldev.com")]
 [assembly: AssemblyProduct("Brutal.Dev.StrongNameSigner.Console")]
-[assembly: AssemblyCopyright("Copyright © 2013-2016")]
+[assembly: AssemblyCopyright("Copyright © 2013-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
