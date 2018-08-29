7ea16ee3c6ac40fee7749d133279b8546bdec924
Werner van Deventer <werner@deventerprise.com>
2018-08-29 15:13:11 +0200

    Version bump.

--- src/Brutal.Dev.StrongNameSigner/Properties/AssemblyInfo.cs
+++ src/Brutal.Dev.StrongNameSigner/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 
 
 // These assemblies are used by Cecil, and reading assemblies with symbols without these DLL's present
