4827105e025fad509d13bdb32a073e00ab3edc1a
Werner van Deventer <werner@brutaldev.com>
2019-09-25 22:00:04 +0200

    Update to remove warnings and run tests in VS2019.

--- src/Brutal.Dev.StrongNameSigner/SigningHelper.cs
+++ src/Brutal.Dev.StrongNameSigner/SigningHelper.cs
@@ -102,7 +102,7 @@ namespace Brutal.Dev.StrongNameSigner
       // Verify assembly path was passed in.
       if (string.IsNullOrWhiteSpace(assemblyPath))
       {
-        throw new ArgumentNullException("assemblyPath");
+        throw new ArgumentNullException(nameof(assemblyPath));
       }
 
       // Make sure the file actually exists.
@@ -126,7 +126,7 @@ namespace Brutal.Dev.StrongNameSigner
       using (var outputFileMgr = new OutputFileManager(assemblyPath, outputFile))
       {
         // Get the assembly info and go from there.
-        AssemblyInfo info = GetAssemblyInfo(assemblyPath);
+        var info = GetAssemblyInfo(assemblyPath);
 
         // Don't sign assemblies with a strong-name signature.
         if (info.IsSigned)
@@ -151,7 +151,7 @@ namespace Brutal.Dev.StrongNameSigner
           ad.Write(outputFileMgr.IntermediateAssemblyPath, new WriterParameters() { StrongNameKeyPair = GetStrongNameKeyPair(keyPath, keyFilePassword), WriteSymbols = outputFileMgr.HasSymbols });
         }
 
-        AssemblyInfoCache.TryRemove(assemblyPath, out KeyValuePair<string, AssemblyInfo> _);
+        AssemblyInfoCache.TryRemove(assemblyPath, out var _);
 
         outputFileMgr.Commit();
 
@@ -174,7 +174,7 @@ namespace Brutal.Dev.StrongNameSigner
       // Verify assembly path was passed in.
       if (string.IsNullOrWhiteSpace(assemblyPath))
       {
-        throw new ArgumentNullException("assemblyPath");
+        throw new ArgumentNullException(nameof(assemblyPath));
       }
 
       // Make sure the file actually exists.
@@ -187,7 +187,7 @@ namespace Brutal.Dev.StrongNameSigner
       if (AssemblyInfoCache.ContainsKey(assemblyPath) && AssemblyInfoCache.TryGetValue(assemblyPath, out a) &&
           !GetFileMD5Hash(assemblyPath).Equals(a.Key, StringComparison.OrdinalIgnoreCase))  // Check if the file contents have changed.
       {
-        AssemblyInfoCache.TryRemove(assemblyPath, out a);
+        AssemblyInfoCache.TryRemove(assemblyPath, out var _);
 
         // Overwrite with a blank version.
         a = new KeyValuePair<string, AssemblyInfo>(null, null);
@@ -259,12 +259,12 @@ namespace Brutal.Dev.StrongNameSigner
       // Verify assembly path was passed in.
       if (string.IsNullOrWhiteSpace(assemblyPath))
       {
-        throw new ArgumentNullException("assemblyPath");
+        throw new ArgumentNullException(nameof(assemblyPath));
       }
 
       if (string.IsNullOrWhiteSpace(referenceAssemblyPath))
       {
-        throw new ArgumentNullException("referenceAssemblyPath");
+        throw new ArgumentNullException(nameof(referenceAssemblyPath));
       }
 
       // Make sure the file actually exists.
@@ -296,7 +296,7 @@ namespace Brutal.Dev.StrongNameSigner
 
             a.Write(fileManagerA.IntermediateAssemblyPath, new WriterParameters { StrongNameKeyPair = GetStrongNameKeyPair(keyPath, keyFilePassword), WriteSymbols = File.Exists(Path.ChangeExtension(assemblyPath, ".pdb")) });
 
-            AssemblyInfoCache.TryRemove(assemblyPath, out KeyValuePair<string, AssemblyInfo> _);
+            AssemblyInfoCache.TryRemove(assemblyPath, out var _);
 
             fixApplied = true;
           }
@@ -314,7 +314,7 @@ namespace Brutal.Dev.StrongNameSigner
             // Save and resign.
             b.Write(fileManagerB.IntermediateAssemblyPath, new WriterParameters { StrongNameKeyPair = GetStrongNameKeyPair(keyPath, keyFilePassword), WriteSymbols = File.Exists(Path.ChangeExtension(referenceAssemblyPath, ".pdb")) });
 
-            AssemblyInfoCache.TryRemove(assemblyPath, out KeyValuePair<string, AssemblyInfo> _);
+            AssemblyInfoCache.TryRemove(assemblyPath, out var _);
 
             fixApplied = true;
           }
@@ -360,7 +360,7 @@ namespace Brutal.Dev.StrongNameSigner
       // Verify assembly path was passed in.
       if (string.IsNullOrWhiteSpace(assemblyPath))
       {
-        throw new ArgumentNullException("assemblyPath");
+        throw new ArgumentNullException(nameof(assemblyPath));
       }
 
       // Make sure the file actually exists.
@@ -379,7 +379,7 @@ namespace Brutal.Dev.StrongNameSigner
           foreach (var friendReference in ivtAttributes)
           {
             // Find any without a public key defined.
-            if (friendReference.HasConstructorArguments && friendReference.ConstructorArguments.Any(ca => ca.Value != null && ca.Value.ToString().IndexOf("PublicKey=", StringComparison.Ordinal) == -1))
+            if (friendReference.HasConstructorArguments && friendReference.ConstructorArguments.Any(ca => ca.Value?.ToString().IndexOf("PublicKey=", StringComparison.Ordinal) == -1))
             {
               a.CustomAttributes.Remove(friendReference);
               fixApplied = true;
@@ -390,7 +390,7 @@ namespace Brutal.Dev.StrongNameSigner
           {
             a.Write(outFileMgr.IntermediateAssemblyPath, new WriterParameters { StrongNameKeyPair = GetStrongNameKeyPair(keyPath, keyFilePassword), WriteSymbols = File.Exists(Path.ChangeExtension(assemblyPath, ".pdb")) });
 
-            AssemblyInfoCache.TryRemove(assemblyPath, out KeyValuePair<string, AssemblyInfo> _);
+            AssemblyInfoCache.TryRemove(assemblyPath, out var _);
           }
         }
 
@@ -408,8 +408,7 @@ namespace Brutal.Dev.StrongNameSigner
         {
           var cert = new X509Certificate2(keyPath, keyFilePassword, X509KeyStorageFlags.Exportable);
 
-          var provider = cert.PrivateKey as RSACryptoServiceProvider;
-          if (provider == null)
+          if (!(cert.PrivateKey is RSACryptoServiceProvider provider))
           {
             throw new InvalidOperationException("The key file is not password protected or the incorrect password was provided.");
           }
@@ -507,215 +506,6 @@ namespace Brutal.Dev.StrongNameSigner
       }
 
       return sb.ToString();
-    }
-
-    /// <summary>
-    /// Utility class that assists in the handling of temporary files during assembly signing. It will create 
-    /// a temporary directory to hold generated files during the signing process, and will move these to their
-    /// final location upon calling <see cref="Commit"/>.  It also ensures that all temporary/intermediate files
-    /// are deleted upon disposing the instance.
-    /// </summary>
-    /// <seealso cref="System.IDisposable" />
-    private sealed class OutputFileManager : IDisposable
-    {
-      private string tempDir;
-
-      public OutputFileManager(string sourceAssemblyPath, string targetAssemblyPath)
-      {
-        SourceAssemblyPath = Path.GetFullPath(sourceAssemblyPath);
-        TargetAssemblyPath = Path.GetFullPath(targetAssemblyPath);
-        IsInPlaceReplace = string.Equals(SourceAssemblyPath, TargetAssemblyPath, StringComparison.Ordinal);
-
-        if (IsInPlaceReplace)
-        {          
-          tempDir = Path.Combine(Path.GetTempPath(), $"StrongNamerTemp.{Process.GetCurrentProcess().Id}.{Path.GetRandomFileName()}");
-          Directory.CreateDirectory(tempDir);
-          IntermediateAssemblyPath = Path.Combine(tempDir, Path.GetFileName(sourceAssemblyPath));
-        }
-        else
-        {
-          IntermediateAssemblyPath = TargetAssemblyPath;
-        }
-
-        HasSymbols = File.Exists(Path.ChangeExtension(SourceAssemblyPath, ".pdb"));
-      }
-
-      ~OutputFileManager()
-      {
-        Dispose();
-      }
-
-      #region Properties
-
-      /// <summary>
-      /// Gets a value indicating whether the source assembly has a matching PDB file.
-      /// </summary>
-      public bool HasSymbols { get; }
-
-      /// <summary>
-      /// Indicates whether the SourceAssemblyPath and the TargetAssemblyPath are equal.
-      /// </summary>
-      public bool IsInPlaceReplace { get; }
-
-      /// <summary>
-      /// Gets the path of the source assembly.
-      /// </summary>
-      public string SourceAssemblyPath { get; }
-
-      /// <summary>
-      /// Gets the path to the .PDB file of the source assembly. (Does not check for existence of the file)
-      /// </summary>
-      public string SourcePdbPath => Path.ChangeExtension(SourceAssemblyPath, ".pdb");
-
-      /// <summary>
-      /// Gets the intermediate path to which the new assembly should be written. This may be the same as <see cref="TargetAssemblyPath"/>,
-      /// if it is different from <see cref="SourceAssemblyPath"/>. (This property is never equal to <see cref="SourceAssemblyPath"/>).
-      /// </summary>
-      public string IntermediateAssemblyPath { get; }
-
-      /// <summary>
-      /// Gets the intermediate path to which the new .PDB should be written. This may be the same as <see cref="TargetAssemblyPath"/>,
-      /// if it is different from <see cref="SourceAssemblyPath"/>.
-      /// </summary>
-      public string IntermediatePdbPath => Path.ChangeExtension(IntermediateAssemblyPath, ".pdb");
-
-      /// <summary>
-      /// Gets the path to where the final output assembly should reside. This may be the same as <see cref="SourceAssemblyPath"/>.
-      /// </summary>
-      public string TargetAssemblyPath { get; }
-
-      /// <summary>
-      /// Gets the path to the .PDB file of the target assembly. (Does not check for existence of the file)
-      /// </summary>
-      public string TargetPdbPath => Path.ChangeExtension(TargetAssemblyPath, ".pdb");
-
-      /// <summary>
-      /// Gets the path to where a backup of the source assembly should be saved.
-      /// </summary>
-      public string BackupAssemblyPath => SourceAssemblyPath + ".unsigned";
-
-      /// <summary>
-      /// Gets the path to where a backup of the source .PDB file should be saved.
-      /// </summary>
-      public string BackupPdbPath => SourcePdbPath + ".unsigned";
-
-      /// <summary>
-      /// This property will be set to true after <see cref="CreateBackup"/> has been called.
-      /// </summary>
-      public bool HasBackup { get; private set; }
-
-      private bool UseTemporaryDirectory => tempDir != null;
-
-      #endregion
-
-      /// <summary>
-      /// Creates a backup of the input files by simply copying them to the <see cref="BackupAssemblyPath"/> and <see cref="BackupPdbPath"/>.
-      /// </summary>
-      public void CreateBackup()
-      {
-        CopyFile(SourceAssemblyPath, BackupAssemblyPath, false);
-
-        if (HasSymbols)
-        {
-          CopyFile(SourcePdbPath, BackupPdbPath, false);
-        }
-
-        HasBackup = true;
-      }
-
-      /// <summary>
-      /// Directly copies <see cref="SourceAssemblyPath"/> and <see cref="SourcePdbPath"/> to <see cref="TargetAssemblyPath"/> 
-      /// and <see cref="TargetPdbPath"/> (if the source files exists).
-      /// </summary>
-      public void CopySourceToFinalOutput()
-      {
-        CopyFile(SourceAssemblyPath, TargetAssemblyPath, false);
-
-        if (HasSymbols)
-        {
-          CopyFile(SourcePdbPath, TargetPdbPath, false);
-        }
-      }
-
-      /// <summary>
-      /// Moves the intermediate files to the target locations if a temporary directory was used during generation. 
-      /// Otherwise this method does nothing.
-      /// </summary>
-      public void Commit()
-      {
-        if (UseTemporaryDirectory)
-        {
-          try
-          {
-            // Only move files if the target assembly to move actually was created.
-            CopyFile(IntermediateAssemblyPath, TargetAssemblyPath, true);
-            if (HasSymbols)
-            {
-              CopyFile(IntermediatePdbPath, TargetPdbPath, true);
-            }
-          }
-          catch (Exception)
-          {
-            if (HasBackup)
-            {
-              // Restore backup
-              CopyFile(BackupAssemblyPath, SourceAssemblyPath, true);
-              CopyFile(BackupPdbPath, SourcePdbPath, true);
-            }
-
-            throw;
-          }
-        }
-        else
-        {
-          // Nothing to commit if we didn't use a temporary directory.
-        }
-      }
-
-      public void Dispose()
-      {
-        GC.SuppressFinalize(this);
-        if (tempDir != null)
-        {
-          try
-          {
-            Directory.Delete(tempDir, true);
-            tempDir = null;
-          }
-          catch
-          {
-            // Ignore errors when attempting to clean up temporary directory.
-          }
-        }
-      }
-
-      /// <summary>
-      /// Copies or moves a single file if it exists. It will always overwrite the target if it exists (provided that
-      /// the source file exists).
-      /// </summary>
-      /// <param name="source">The source file to copy/move if it exists.</param>
-      /// <param name="target">The target file to write to.</param>
-      /// <param name="move">if set to <see langword="true" /> the file is moved, otherwise it is copied.</param>
-      private static void CopyFile(string source, string target, bool move)
-      {
-        if (File.Exists(source))
-        {
-          if (File.Exists(target))
-          {
-            File.SetAttributes(target, FileAttributes.Normal);
-            File.Delete(target);
-          }
-
-          if (move)
-          {
-            File.Move(source, target);
-          }
-          else
-          {
-            File.Copy(source, target);
-          }
-        }
-      }
     }
   }
 }
