c8ea46bc5927577912c0d539d9898222a898db15
Werner van Deventer <werner@deventerprise.com>
2023-01-05 22:09:54 +0200

    Version and year bump.

--- src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
+++ src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://brutaldev.com")]
 [assembly: AssemblyProduct("Brutal.Dev.StrongNameSigner.Console")]
-[assembly: AssemblyCopyright("Copyright © 2013-2022")]
+[assembly: AssemblyCopyright("Copyright © 2013-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.2.0")]
-[assembly: AssemblyFileVersion("3.2.2.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
