4972b1ce0501d4bfad867bba65d0eb89b51d9dcc
Oliver Warrick <Oliver.Warrick@wisetechglobal.com>
2023-10-04 16:12:27 +1100

    remove unnesisary usings

--- src/Brutal.Dev.StrongNameSigner.Tests/SignAssemblyTests.cs
+++ src/Brutal.Dev.StrongNameSigner.Tests/SignAssemblyTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
-using System.Threading;
 using System.Threading.Tasks;
 using Shouldly;
 using Xunit;
