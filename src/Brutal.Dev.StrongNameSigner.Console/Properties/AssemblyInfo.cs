d47c74cf8d92ab40c8e35560783164d86ea83f45
Werner van Deventer <werner@deventerprise.com>
2026-02-17 22:34:42 +0000

    Update packages and version.

--- src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
+++ src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://brutaldev.com")]
 [assembly: AssemblyProduct("Brutal.Dev.StrongNameSigner.Console")]
-[assembly: AssemblyCopyright("Copyright © 2013-2023")]
+[assembly: AssemblyCopyright("Copyright © 2013-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.3.0")]
-[assembly: AssemblyFileVersion("3.6.3.0")]
+[assembly: AssemblyVersion("3.6.4.0")]
+[assembly: AssemblyFileVersion("3.6.4.0")]
